0e6f27ce3fadcb7d74e44909e16c0af4f3232f44
BFAlex <838318741@qq.com>
2017-09-19 10:47:27 +0800

    Initial Commit

---
+++ BFSwift2Command/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BFSwift2Command
+//
+//  Created by Readboy_BFAlex on 2017/9/19.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

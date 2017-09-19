96e26babb3af7117454a7c4735fe659536b2972e
BFAlex <838318741@qq.com>
2017-09-19 11:16:04 +0800

    Swift
    使用Command来实现逻辑指令

---
+++ BFSwift2Command/Base(基础)/BFBaseObject.swift
@@ -0,0 +1,14 @@
+//
+//  BFBaseObject.swift
+//  BFSwift2Command
+//
+//  Created by Readboy_BFAlex on 2017/9/19.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+import Cocoa
+
+class BFBaseObject: NSObject {
+
+    
+}
--- BFSwift2Command/main.swift
+++ BFSwift2Command/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
+//MARK:- 输出
 print("Hello, World!")
+print("Hello, Swift2.x")
+print("\n")
+
+//MARK:- 简单值
+// 变量
+var num1 = 10
+print("变量初始值:num1 = \(num1)")
+num1 = 20
+print("变量改变值:num1 = \(num1)")
+print("\n")
+// 常量
+let num2 = 30
+print("常量值:num2 = \(num2) 赋值后不能改变值")
+let numInt = 10
+let numDouble1 = 10.0
+let numDouble2: Double = 10
+let numFloat: Float = 20
+print("numInt:\(numInt)")
+print("numDouble1:\(numDouble1)")
+print("numDouble2:\(numDouble2)")
+print("numFloat:\(numFloat)")
+print("\n")
+
+
+
 
